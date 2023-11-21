cb0e500a376ee55e1fcd064e5e2657f94eeb1ad8
Joseph Olaniyan <josepholaniyan88@gmail.com>
2023-11-20 21:31:59 -0500

    final commit

--- backend/project4.py
+++ backend/project4.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from sklearn.preprocessing import StandardScaler
+from joblib import load
 import pickle
 import numpy as np
 from flask import request
@@ -11,16 +13,21 @@ CORS(app)
 
 # load model
 loaded_rf_model = pickle.load(open('../DataProcessing/rf_model.pickle', "rb"))
+loaded_scaler = load('../DataProcessing/std_scaler.bin')
 
 
 @app.route("/input", methods=['POST'])
 def input():
     data = request.get_json()
-    print(data)
+    # x_array = np.array(data)
+    # print(x_array)
+    x_array = loaded_scaler.transform(data)
+    print(x_array)
     output = []
-    result = loaded_rf_model.predict(data)
+    result = loaded_rf_model.predict(x_array)
     print(result[0])
-    return jsonify(data)
+    output.append({'value': result[0]})
+    return jsonify(output)
 
 
 if __name__ == '__main__':
